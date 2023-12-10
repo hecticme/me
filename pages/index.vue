ce7e3093c8a845fb6a69cd942a7f277e1400d9a8
hecticme <hangoc.minh1306@gmail.com>
2023-12-10 21:24:06 +0700

    Add welcome text for homepage

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,40 @@
+<script setup lang="ts"></script>
+
 <template>
-  <h1>Landing page</h1>
+  <section class="welcome-message">
+    <div class="welcome-message__say-hi">
+      <h1>Hi! I'm Minh.</h1>
+      <h1>a web developer.</h1>
+    </div>
+
+    <p class="welcome-message__more-description">
+      I'm also a designer with a perceptive vision for bringing engaging UI to
+      life.
+    </p>
+  </section>
 </template>
 
-<script setup>
+<style lang="scss" scoped>
+.welcome-message {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 1rem;
 
-</script>
+  &__say-hi {    
+    h1 {
+      font-family: var(--header-font);
+      font-size: 4rem;
+      font-weight: 700;
+      text-align: center;
+      line-height: 1.3;
+    }
+  }
+
+  &__more-description {
+    width: 20rem;
+    text-align: center;
+    line-height: 1.4;
+  }
+}
+</style>
