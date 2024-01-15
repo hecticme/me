701dbbfc506886a73862943d2d0c36cbc0d86b5a
hecticme <hangoc.minh1306@gmail.com>
2024-01-15 22:10:50 +0700

    Add initial `HomeProject` components

---
+++ components/HomeProjectCard.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="project-card">
+
+  </div>
+</template>
+
+<style scoped>
+.project-card {
+  width: 720px;
+  height: 360px;
+  border-radius: 8px;
+  box-shadow: 0px 0px 50px 0px rgba(0, 0, 0, 0.15);
+}
+</style>
---
+++ components/HomeProjects.vue
@@ -0,0 +1,15 @@
+<template>
+  <section class="projects-container">
+    <HomeProjectCard />
+  </section>
+</template>
+
+<style scoped>
+.projects-container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 80px;
+  width: 100%;
+}
+</style>
