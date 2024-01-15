6fa04e6751e0b77d18f5a7f6ca074d84f96852c4
hecticme <hangoc.minh1306@gmail.com>
2024-01-15 22:11:21 +0700

    Restructure homepage component
    This commit moved welcome message to a separate component

---
+++ components/HomeWelcome.vue
@@ -0,0 +1,36 @@
+<template>
+  <section class="welcome-message">
+    <div class="welcome-message__say-hi">
+      <h1>Hi! I'm Minh.</h1>
+      <h1>a web developer.</h1>
+    </div>
+
+    <div class="welcome-message__description">
+      <p>I'm also a designer with a perceptive vision</p>
+      <p>for bringing engaging UI to life.</p>
+    </div>
+  </section>
+</template>
+
+<style scoped>
+.welcome-message {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 1rem;
+  margin-block-end: 5rem;
+}
+
+.welcome-message__say-hi {
+  font-family: var(--header-font);
+  font-size: 4rem;
+  font-weight: 700;
+  text-align: center;
+  line-height: 1.3;
+}
+
+.welcome-message__description {
+  text-align: center;
+  line-height: 1.4;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,37 +1,5 @@
-<script setup lang="ts"></script>
-
 <template>
-  <section class="welcome-message">
-    <div class="welcome-message__say-hi">
-      <h1>Hi! I'm Minh.</h1>
-      <h1>a web developer.</h1>
-    </div>
+  <HomeWelcome />
 
-    <div class="welcome-message__description">
-      <p>I'm also a designer with a perceptive vision</p>
-      <p>for bringing engaging UI to life.</p>
-    </div>
-  </section>
+  <HomeProjects />
 </template>
-
-<style scoped>
-.welcome-message {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  gap: 1rem;
-}
-
-.welcome-message__say-hi {
-  font-family: var(--header-font);
-  font-size: 4rem;
-  font-weight: 700;
-  text-align: center;
-  line-height: 1.3;
-}
-
-.welcome-message__description {
-  text-align: center;
-  line-height: 1.4;
-}
-</style>
