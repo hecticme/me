72222e8963b5aeede1c6a6f2abde73e6cf745135
hecticme <hangoc.minh1306@gmail.com>
2024-01-15 21:45:46 +0700

    Replace scss styles in homepage with css

--- pages/index.vue
+++ pages/index.vue
@@ -7,34 +7,31 @@
       <h1>a web developer.</h1>
     </div>
 
-    <p class="welcome-message__more-description">
-      I'm also a designer with a perceptive vision for bringing engaging UI to
-      life.
-    </p>
+    <div class="welcome-message__description">
+      <p>I'm also a designer with a perceptive vision</p>
+      <p>for bringing engaging UI to life.</p>
+    </div>
   </section>
 </template>
 
-<style lang="scss" scoped>
+<style scoped>
 .welcome-message {
   display: flex;
   flex-direction: column;
   align-items: center;
   gap: 1rem;
+}
 
-  &__say-hi {
-    h1 {
-      font-family: var(--header-font);
-      font-size: 4rem;
-      font-weight: 700;
-      text-align: center;
-      line-height: 1.3;
-    }
-  }
+.welcome-message__say-hi {
+  font-family: var(--header-font);
+  font-size: 4rem;
+  font-weight: 700;
+  text-align: center;
+  line-height: 1.3;
+}
 
-  &__more-description {
-    width: 20rem;
-    text-align: center;
-    line-height: 1.4;
-  }
+.welcome-message__description {
+  text-align: center;
+  line-height: 1.4;
 }
 </style>
