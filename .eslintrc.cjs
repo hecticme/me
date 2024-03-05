37898c6a8327e6b22df72e976e6eb6f3d0ab439c
hecticme <hangoc.minh1306@gmail.com>
2024-03-05 23:03:21 +0700

    chore(lint): add `vue/html-closing-bracket-newline` rule

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -135,6 +135,9 @@ module.exports = {
     'vue/first-attribute-linebreak': [
       'error',
     ],
+    'vue/html-closing-bracket-newline':[
+      'error',
+    ],
     'vue/html-indent': [
       'error',
       2,
