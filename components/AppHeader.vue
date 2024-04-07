c12da1a336418fc252c915a8c191e6a1fbd8d1f4
hecticme <hangoc.minh1306@gmail.com>
2024-04-07 13:14:13 +0700

    style: update header UI

--- components/AppHeader.vue
+++ components/AppHeader.vue
@@ -6,15 +6,18 @@ const navLinks = ref([
 </script>
 
 <template>
-  <header>
+  <header class="sticky bg-white shadow-outline flex justify-between top-5 mx-auto px-8 py-3 max-w-lg rounded-full">
     <div>
-      <NuxtLink to="/">
-        hecticme.
+      <NuxtLink
+        class="font-semibold"
+        to="/"
+      >
+        me.
       </NuxtLink>
     </div>
 
     <nav>
-      <ul>
+      <ul class="flex gap-4">
         <li
           v-for="(link, index) in navLinks"
           :key="index"
