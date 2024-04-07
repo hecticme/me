87390b32f89457fa1ffb6db1055f3de53557c646
hecticme <hangoc.minh1306@gmail.com>
2024-04-07 13:13:26 +0700

    style: add outline shadow for Tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,11 @@ export default {
     './error.vue',
   ],
   theme: {
-    extend: {},
+    extend: {
+      boxShadow:{
+        outline: 'rgba(0, 0, 0, 0.05) 0px 6px 24px 0px, rgba(0, 0, 0, 0.08) 0px 0px 0px 1px',
+      },
+    },
   },
   plugins: [],
 }
