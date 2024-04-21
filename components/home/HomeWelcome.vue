acd85d2e28b46a9d70cd19344d45373b1eff5b61
hecticme <hangoc.minh1306@gmail.com>
2024-04-21 18:37:14 +0700

    style(HomeWelcome): update layout

--- components/home/HomeWelcome.vue
+++ components/home/HomeWelcome.vue
@@ -1,13 +1,33 @@
 <template>
-  <section>
-    <div>
-      <h1>Hi! I'm Minh.</h1>
-      <h1>a web developer.</h1>
+  <section class="max-w-7xl mx-auto">
+    <h1 class="font-serif font-bold text-4xl">
+      hecticme
+    </h1>
+
+    <p class="mt-6">
+      Hi 👋! I'm Ngọc Minh. A person with a lot of problems.
+    </p>
+
+    <div class="flex flex-col gap-4 mt-10">
+      <p>
+        Having challenged myself with illustration and design in the past, <br>
+        I'm also considered a designer with an obsession with details.
+      </p>
+
+      <p>
+        Outside of programming, I enjoy travelling and playing RPG video games. <br>
+        None of that for now though, at least that's what I'm striving to do.
+      </p>
     </div>
 
-    <div>
-      <p>I'm also a designer with a perceptive vision</p>
-      <p>for bringing engaging UI to life.</p>
+    <div class="flex flex-col gap-1 mt-10">
+      <p>Connect with me through:</p>
+
+      <div class="flex gap-1">
+        <p>1</p>
+        <p>2</p>
+        <p>3</p>
+      </div>
     </div>
   </section>
 </template>
