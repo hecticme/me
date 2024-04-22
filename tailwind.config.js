3f64f0c3aacb6cb2f9609117dff1d0c910a03cc8
hecticme <hangoc.minh1306@gmail.com>
2024-04-22 11:45:23 +0700

    chore: remove outline box-shadow

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,9 +12,6 @@ export default {
   ],
   theme: {
     extend: {
-      boxShadow:{
-        outline: 'rgba(0, 0, 0, 0.05) 0px 6px 24px 0px, rgba(0, 0, 0, 0.08) 0px 0px 0px 1px',
-      },
       fontFamily: {
         sans: ['Space Grotesk', ...defaultTheme.fontFamily.sans],
         serif: ['Playfair Display', ...defaultTheme.fontFamily.serif],
