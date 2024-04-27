ebaa16a68e9c0f1be53dff3ae065910fd0b1bc44
H. Ngọc Minh <hangoc.minh1306@gmail.com>
2024-04-27 23:49:55 +0700

    style(TheHeader): add a wrapper for inner content
    Use a div as a wrapper for the inner content of header. This is to set the max-width of the content, make sure the header stays in the middle of the screen.

--- components/TheHeader.vue
+++ components/TheHeader.vue
@@ -17,50 +17,50 @@ const socialLinks = ref([
 </script>
 
 <template>
-  <header class="sticky bg-white flex justify-between items-center top-0 px-8 py-1 border-b border-neutral-900 h-header-full">
-    <div>
-      <NuxtLink
-        class="font-semibold"
-        to="/"
-      >
-        me.
-      </NuxtLink>
-    </div>
-
-    <nav>
-      <ul class="flex gap-8 uppercase">
+  <header class="sticky bg-white top-0 border-b border-gray-900">
+    <div class="flex justify-between items-center h-header-full body-container">
+      <div>
+        <NuxtLink
+          class="font-semibold"
+          to="/"
+        >
+          me.
+        </NuxtLink>
+      </div>
+      <nav>
+        <ul class="flex gap-8 uppercase">
+          <li
+            v-for="(link, index) in navLinks"
+            :key="index"
+          >
+            <NuxtLink
+              active-class="text-gray-900 after:scale-100"
+              :class="[
+                'relative flex justify-center text-gray-400 transition-colors duration-300',
+                'after:absolute after:top-[110%] after:w-[5px] after:h-[5px] after:rounded-full after:bg-gray-900 after:scale-0 after:transition-transform after:duration-300',
+              ]"
+              :to="link"
+            >
+              {{ link }}
+            </NuxtLink>
+          </li>
+        </ul>
+      </nav>
+      <ul class="flex gap-3 items-center">
         <li
-          v-for="(link, index) in navLinks"
+          v-for="(link, index) of socialLinks"
           :key="index"
         >
           <NuxtLink
-            active-class="text-gray-900 after:scale-100"
-            :class="[
-              'relative flex justify-center text-gray-400 transition-colors duration-300',
-              'after:absolute after:top-[110%] after:w-[5px] after:h-[5px] after:rounded-full after:bg-gray-900 after:scale-0 after:transition-transform after:duration-300',
-            ]"
-            :to="link"
+            class="rounded-full flex justify-center items-center aspect-square p-2 transition-colors duration-300 hover:bg-gray-200"
+            rel="noopener noreferrer"
+            target="_blank"
+            :to="link.href"
           >
-            {{ link }}
+            <Icon :name="link.icon" />
           </NuxtLink>
         </li>
       </ul>
-    </nav>
-
-    <ul class="flex gap-3 items-center">
-      <li
-        v-for="(link, index) of socialLinks"
-        :key="index"
-      >
-        <NuxtLink
-          class="rounded-full flex justify-center items-center aspect-square p-2 transition-colors duration-300 hover:bg-gray-200"
-          rel="noopener noreferrer"
-          target="_blank"
-          :to="link.href"
-        >
-          <Icon :name="link.icon" />
-        </NuxtLink>
-      </li>
-    </ul>
+    </div>
   </header>
 </template>
