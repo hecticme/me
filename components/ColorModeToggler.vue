0a79bb148762adc3eb2da21b4c8ab7d7ddbdebe5
H. Ngọc Minh <hangoc.minh1306@gmail.com>
2024-05-01 19:39:55 +0700

    feat: create <ColorModeToggler>

---
+++ components/ColorModeToggler.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+const colorMode = useColorMode()
+
+function changeColorMode () {
+  if (colorMode.value === 'dark') {
+    colorMode.preference = 'light'
+  } else {
+    colorMode.preference = 'dark'
+  }
+}
+</script>
+
+<template>
+  <button
+    class="hover:bg-gray-200 p-2 rounded-full transition-colors duration-300"
+    @click="changeColorMode"
+  >
+    <span class="flex dark:hidden items-center">
+      <Icon name="ph:sun-duotone" />
+    </span>
+
+    <span class="hidden dark:flex dark:items-center">
+      <Icon name="ph:moon-duotone" />
+    </span>
+  </button>
+</template>
