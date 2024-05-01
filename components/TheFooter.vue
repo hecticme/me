c667d5de23ee3350ed5f8fa47bbd23be9df620a8
H. Ngọc Minh <hangoc.minh1306@gmail.com>
2024-05-01 20:06:16 +0700

    style(TheFooter): implement dark mode colors

--- components/TheFooter.vue
+++ components/TheFooter.vue
@@ -7,13 +7,13 @@ const copyright = computed(() => {
 
 <template>
   <footer class="flex justify-center py-4">
-    <p class="flex gap-2 text-sm text-gray-700">
-      <span>
+    <p class="flex gap-2 text-sm text-gray-700 dark:text-gray-300">
+      <span class="transition-colors duration-300">
         {{ copyright }}
       </span>
 
       <NuxtLink
-        class="font-medium underline underline-offset-2 hover:text-gray-500 transition-colors duration-300"
+        class="font-medium underline underline-offset-2 hover:text-gray-500 transition-colors duration-300 dark:hover:text-gray-100"
         rel="noopener noreferrer"
         target="_blank"
         to="https://creativecommons.org/licenses/by-sa/4.0/"
