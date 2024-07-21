e69ca8339073bdc41a98f31062446ede0bee2b95
H. Ngọc Minh <hangoc.minh1306@gmail.com>
2024-07-21 23:19:22 +0700

    style(TheLogo): add hover color in dark mode

--- components/TheLogo.vue
+++ components/TheLogo.vue
@@ -5,7 +5,7 @@
 <template>
   <NuxtLink
     :class="[
-      'flex font-medium transition-colors text-gray-400 hover:text-gray-700',
+      'flex font-medium transition-colors text-gray-400 hover:text-gray-700 dark:hover:text-gray-200',
       {
         'text-gray-900 dark:text-white': $route.name === 'index',
       }
