6cb8202ab991ec202b4f4159bf221d7cd19d6851
H. Ngọc Minh <hangoc.minh1306@gmail.com>
2024-07-24 00:26:02 +0700

    style(TheHeader): change background color

--- components/TheHeader.vue
+++ components/TheHeader.vue
@@ -17,7 +17,7 @@ const socialLinks = ref([
 </script>
 
 <template>
-  <header class="sticky bg-white dark:bg-gray-900 top-0 border-b border-gray-300 dark:border-gray-700 transition-colors">
+  <header class="sticky bg-gray-50 dark:bg-gray-950 top-0 border-b border-gray-300 dark:border-gray-700 transition-colors">
     <div class="flex justify-between items-center h-16 body-container">
       <div>
         <TheLogo />
