74c8cfbe9a8bcd382017bb99d500177b5dd385f9
H. Ngọc Minh <hangoc.minh1306@gmail.com>
2024-07-27 02:09:45 +0700

    style(TheHeader): use min height instead of fixed height

--- components/TheHeader.vue
+++ components/TheHeader.vue
@@ -18,7 +18,7 @@ const socialLinks = ref([
 
 <template>
   <header class="sticky bg-gray-50 dark:bg-gray-950 top-0 border-b border-gray-300 dark:border-gray-700 transition-colors">
-    <div class="flex justify-between items-center h-16 body-container">
+    <div class="flex justify-between items-center min-h-16 body-container">
       <div>
         <TheLogo />
       </div>
