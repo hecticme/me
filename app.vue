c523973f3fbfe248d590d34d6d9c6f3ab3bf981e
H. Ngọc Minh <hangoc.minh1306@gmail.com>
2024-07-31 23:29:08 +0700

    style: apply transition between pages

--- app.vue
+++ app.vue
@@ -28,3 +28,16 @@ useHead({
 
   <div id="teleports" />
 </template>
+
+<style>
+.page-enter-active,
+.page-leave-active {
+  transition: all 250ms cubic-bezier(0.4, 0, 0.2, 1);
+}
+
+.page-enter-from,
+.page-leave-to {
+  transform: translateY(6px);
+  opacity: 0;
+}
+</style>
