a78b6ec987cc0ff7722fdd08b83d81b358cf475b
H. Ngọc Minh <hangoc.minh1306@gmail.com>
2024-08-23 22:53:55 +0700

    feat: display blog's title and datetime for each blog

--- pages/blog/[...slug].vue
+++ pages/blog/[...slug].vue
@@ -1,5 +1,40 @@
+<script setup lang="ts">
+const route = useRoute()
+
+const { data: blogDetails } = await useAsyncData(
+  route.fullPath,
+  () => queryContent(route.fullPath)
+    .only(['title', 'date'])
+    .findOne()
+)
+
+const { format: formatDate } = new Intl.DateTimeFormat('en-US', {
+  month: 'long',
+  day: '2-digit',
+  year: 'numeric',
+})
+</script>
+
 <template>
   <div class="body-container">
-    <ContentDoc />
+    <section>
+      <h2 class="font-semibold text-3xl">
+        {{ blogDetails?.title }}
+      </h2>
+
+      <time
+        class="
+          mt-2
+          text-xs
+          text-gray-700 dark:text-gray-300
+          transition-colors
+        "
+        :datetime="blogDetails?.date"
+      >
+        {{ formatDate(new Date(blogDetails?.date)) }}
+      </time>
+    </section>
+
+    <ContentDoc class="mt-8 leading-relaxed" />
   </div>
 </template>
