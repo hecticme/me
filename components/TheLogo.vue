49d5900133f52b2c988c0358c2d8e7e779186b88
H. Ngọc Minh <hangoc.minh1306@gmail.com>
2024-10-03 20:43:20 +0700

    chore: use `active-class` for NuxtLink
    This commit used `active-class` of NuxtLink instead of manually checking the route name.

--- components/TheLogo.vue
+++ components/TheLogo.vue
@@ -4,11 +4,9 @@
 
 <template>
   <NuxtLink
+    active-class="text-gray-900 dark:text-white"
     :class="[
       'flex font-medium transition-colors text-gray-400 hover:text-gray-700 dark:hover:text-gray-200',
-      {
-        'text-gray-900 dark:text-white': $route.name === 'index',
-      }
     ]"
     to="/"
   >
