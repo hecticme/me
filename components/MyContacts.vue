4d7cc78f048b12cfd2ef9562d7c5410bd1425cb2
H. Ngọc Minh <hangoc.minh1306@gmail.com>
2024-10-25 18:41:05 +0700

    chore: code format

--- components/MyContacts.vue
+++ components/MyContacts.vue
@@ -21,7 +21,7 @@ const contracts = [
 <template>
   <div class="flex flex-col gap-1 w-fit">
     <NuxtLink
-      v-for="(contract ,index) of contracts"
+      v-for="(contract, index) of contracts"
       :key="index"
       class="font-medium underline underline-offset-2 flex items-center gap-1 p-1 text-zinc-900 bg-transparent hover:bg-zinc-200 rounded transition-colors dark:text-white dark:hover:bg-zinc-700"
       rel="noopener noreferrer"
