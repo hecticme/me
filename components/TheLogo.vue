1503a8f98f96d09b5618a2df5e02add8fa62339f
H. Ngọc Minh <hangoc.minh1306@gmail.com>
2024-10-25 18:26:24 +0700

    style(TheLogo): change base and hover color

--- components/TheLogo.vue
+++ components/TheLogo.vue
@@ -6,7 +6,7 @@
   <NuxtLink
     active-class="text-zinc-900 dark:text-white"
     :class="[
-      'flex font-medium transition-colors text-zinc-400 hover:text-zinc-700 dark:hover:text-zinc-200',
+      'flex font-medium transition-colors text-zinc-500 hover:text-zinc-700 dark:hover:text-zinc-300',
     ]"
     to="/"
   >
