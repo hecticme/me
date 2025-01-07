aaea5129ab51e2ab4bf4f8a89b7759882eeb324b
H. Ngọc Minh <hangoc.minh1306@gmail.com>
2025-01-07 23:35:45 +0700

    chore: edit homepage content

--- pages/index.vue
+++ pages/index.vue
@@ -7,13 +7,11 @@ useSeoMeta({
 <template>
   <section class="body-container">
     <h1 class="font-bold text-2xl">
-      Hi 👋! I'm Ngọc Minh.
+      Hello! I'm Ngọc Minh.
     </h1>
 
     <div class="mt-6 flex flex-col gap-2">
-      <p>I do web stuff for a living.</p>
-
-      <p>There's not much to tell about me right now...</p>
+      <p>I do web stuff for a living, mostly on the Frontend.</p>
     </div>
 
     <Divider />
